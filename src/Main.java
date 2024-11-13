166d37752b9a347d88f92392aab178b8610a81e8
drewenia <egedata@gmail.com>
2024-11-13 21:40:02 +0300

    Chapter 01

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-import java.util.Arrays;
+import java.lang.System;
+import java.lang.*;
+import java.util.Random;
+import java.util.random.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
     }
 }
